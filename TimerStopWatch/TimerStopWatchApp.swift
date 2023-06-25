f2ac344533abe4a21880403b4b6dd0aefc913c2b
Alex Hudson <AHudson@tql.com>
2023-06-24 21:17:58 -0400

    Initial Commit

---
+++ TimerStopWatch/TimerStopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerStopWatchApp.swift
+//  TimerStopWatch
+//
+//  Created by Alex Hudson on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerStopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
